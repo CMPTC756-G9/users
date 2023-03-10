9313733fa2bfe3184e1a326835f96708c67a4f48
Nima Modares Gorji <nima.mg@gmail.com>
2023-03-09 20:54:13 -0800

    Remove commented out serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,29 +1,11 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from users.models import User
-
-# class UserSerializer(serializers.Serializer):
-#     first_name = serializers.CharField()
-#     last_name = serializers.CharField()
-#     username = serializers.CharField()
-#     password = serializers.CharField()
-#     email = serializers.EmailField()
-
-
-    # def update(self, instance, validated_data):
-    #     for k, v in validated_data.items():
-    #         if hasattr(instance, k):
-    #             setattr(instance, k, v)
-    #         else:
-    #             raise ValidationError(detail=f'User does not have the attribute: {k}')
-    #     instance.save()
-    #     return instance
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        lookup_field = 'slug'
+        lookup_field = 'username'
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'password', 'email')
 
