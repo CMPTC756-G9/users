7fb9fd3599a8c9cd39d3b5d82f08866b3797eeb2
Nima Modares Gorji <nima.mg@gmail.com>
2023-03-11 00:36:36 -0800

    Change DB env var names

--- distributed/settings.py
+++ distributed/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'distributed.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('db_name', ''),
-        'USER': os.environ.get('db_user', ''),
-        'PASSWORD': os.environ.get('db_pass', ''),
-        'HOST': os.environ.get('db_host', 'localhost'),
-        'PORT': os.environ.get('db_port', '5432'),
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
