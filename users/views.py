4fbabd60d18f9b4aba40a70a60417f23a9e2ac64
Nima Modares Gorji <nima.mg@gmail.com>
2023-03-12 01:34:50 -0800

    Create financial account after creating user

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
+import os
+
+import requests
 from rest_framework.viewsets import ModelViewSet
+from dotenv import load_dotenv
 
 from users.models import User
 from users.serializers import UserSerializer
+
+load_dotenv()
+
+CREATE_ACCOUNT_URL = 'http://{}/api/accounts/'.format(os.environ.get('FINANCIAL_ROOT_URL'))
 
 
 class UserViewset(ModelViewSet):
@@ -9,5 +17,12 @@ class UserViewset(ModelViewSet):
     serializer_class = UserSerializer
     lookup_field = 'username'
 
-
-
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        account_data = requests.post(
+            CREATE_ACCOUNT_URL,
+            json={
+                'user_id': serializer.data.get('id')
+            }
+        ).json()
+        print('yo')
